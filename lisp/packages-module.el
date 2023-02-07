cb5809c450f841b5143ccaaad5975ae388a5bd69
K8X1D <k8x1d@protonmail.ch>
2023-02-07 18:11:22 -0500

    upd to melpa (better compat with emacs 30)

--- lisp/packages-module.el
+++ lisp/packages-module.el
@@ -5,28 +5,33 @@
 ;; Package set-up
 ;;
 
-;; Load required modules
-(require 'package)
 
-;; Set repos
-(add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(use-package emacs
+  :defer t
+  :config
+  ;; Load required modules
+  (require 'package)
 
-(customize-set-variable 'package-archive-priorities
-                        '(("gnu"    . 80)   ; prefer GNU packages
-                          ("nongnu" . 70)   ; use non-gnu packages if
-                                            ; not found in GNU elpa
-                          ("stable" . 90)   ; prefer "released" versions
-                                            ; from melpa
-                          ("melpa"  . 0)))  ; if all else fails, get it
-                                            ; from melpa
+  ;; Set repos
+  (add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-;; Not sure if necessary...
-;;(when (not package-archive-contents)
-;;    (package-refresh-contents))
+  (customize-set-variable 'package-archive-priorities
+                          '(("gnu"    . 80)   ; prefer GNU packages
+                            ("nongnu" . 70)   ; use non-gnu packages if
+					; not found in GNU elpa
+                            ("stable" . 90)   ; prefer "released" versions
+					; from melpa
+                            ("melpa"  . 99)))  ; if all else fails, get it
+					; from melpa
 
-;; Ensure package is installed
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+  ;; Not sure if necessary...
+  ;;(when (not package-archive-contents)
+  ;;    (package-refresh-contents))
+
+  ;; Ensure package is installed
+  (require 'use-package-ensure)
+  (setq use-package-always-ensure t)
+  )
 
 (provide 'packages-module)
