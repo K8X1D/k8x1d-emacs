1ed68ccff6c50ab970657ec9624c9ac0faf61678
K8X1D <k8x1d@protonmail.ch>
2023-02-08 21:13:36 -0500

    adjust colors

--- lisp/gui-module.el
+++ lisp/gui-module.el
@@ -14,17 +14,22 @@
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom treemacs theme (all-the-icons must be installed!)
+
   (setq doom-themes-treemacs-theme "doom-gruvbox") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-(custom-set-faces
- `(cursor ((t (:background ,(doom-color 'fg)))))
-`(org-todo-keyword-faces
-      '(("WAITING" . (:foreground ,(doom-color 'yellow)))
-        ("CANCELLED" . (:foreground ,(doom-color 'red)))))
+  (doom-themes-org-config)
+  )
 
- )
+
+  ;; Customizations
+  (custom-set-faces
+   `(cursor ((t (:background ,(doom-color 'fg)))))
+   `(org-todo-keyword-faces
+     '(("WAITING" . (:foreground ,(doom-color 'yellow)))
+       ("CANCELLED" . (:foreground ,(doom-color 'red)))))
+   `(corfu-current ((t (:background ,(doom-color 'red)))))
+   )
 
 
 ;; Modeline
