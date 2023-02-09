a4dcdb16101ae93c487eb4ca44406fb4bd25f1e0
K8X1D <k8x1d@protonmail.ch>
2023-02-09 10:46:52 -0500

    adjust citar support

--- lisp/text-module.el
+++ lisp/text-module.el
@@ -32,17 +32,36 @@
 ;;
 
 (use-package citar
-  :defer t
   :bind (("C-c b" . citar-insert-citation)
-	 :map minibuffer-local-map
+	 (:map minibuffer-local-map
 	 ("M-b" . citar-insert-preset))
+	 (:map org-mode-map
+	 ("C-c b" . org-cite-insert))
+	 )
   :config
-  (setq org-cite-global-bibliography '("~/Zotero/k8x1d.bib"))
+  ;; Org cite configuration to use citar
   (setq org-cite-insert-processor 'citar)
   (setq org-cite-follow-processor 'citar)
   (setq org-cite-activate-processor 'citar)
   (setq citar-bibliography org-cite-global-bibliography)
+  (setq citar-at-point-function 'embark-act)
+  ;; Icons support
+  (setq citar-symbols
+ 	`((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+ 	  (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+ 	  (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+  (setq citar-symbol-separator "  ")
+  ;; Notes
+  (setq citar-notes-paths '("~/Zotero/notes"))
   )
+
+
+
+(use-package citar-embark
+  :after citar embark
+  :no-require
+  :config (citar-embark-mode))
+
 
 ;;
 ;; Latex better support
@@ -81,7 +100,6 @@
 	reftex-plug-into-AUCTeX t)
   (add-hook 'TeX-after-compilation-finished-functions
 	    #'TeX-revert-document-buffer)
-  (setq )
   :hook
   ((LaTeX-mode . eglot-ensure)
    (LaTeX-mode . LaTeX-math-mode)
