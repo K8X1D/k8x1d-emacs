2c261b3c18c8ee502ade195cccd22ddc21923d1b
K8X1D <k8x1d@protonmail.ch>
2023-02-09 10:46:03 -0500

    test embark and marginalia

--- lisp/completion-module.el
+++ lisp/completion-module.el
@@ -100,5 +100,34 @@
   (add-to-list 'completion-at-point-functions #'cape-keyword-capf))
 
 
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+
+;;
+;; In test
+;;
+
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 (provide 'completion-module)
