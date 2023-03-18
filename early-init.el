26afa264daed6b461a38464a2e170f5656bf1497
Kevin Kaiser <k8x1d@protonmail.ch>
2023-03-17 22:01:37 -0400

    Add early-init config

---
+++ early-init.el
@@ -0,0 +1,23 @@
+
+;;
+;; Disable package.el in favor of guix
+;;
+(setq package-enable-at-startup nil)
+
+;;
+;;; UI configuration
+;;
+;; Remove some unneeded UI elements (the user can turn back on anything they wish)
+;; https://github.com/SystemCrafters/crafted-emacs/blob/master/early-init.el
+(setq inhibit-startup-message t)
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
+
+;;
+;; Set Theme
+;; 
+(load-theme 'modus-vivendi)
