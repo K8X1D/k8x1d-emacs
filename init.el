06471be2b068ff2effecf26f638a42e999b9c0bc
Kevin Kaiser <k8x1d@protonmail.ch>
2023-03-20 11:50:25 -0400

    add minimal latex support

--- init.el
+++ init.el
@@ -233,6 +233,10 @@
 (setq org-pomodoro-long-break-length 30)
 
 
+;; Bibliography
+(setq org-cite-global-bibliography '("~/Zotero/k8x1d.bib"))
+
+
 ;; GTD
 ;; From https://lucidmanager.org/productivity/getting-things-done-with-emacs/
 ;; From https://members.optusnet.com.au/~charles57/GTD/orgmode.html
@@ -290,3 +294,119 @@
 (setq auth-sources '("~/.authinfo.gpg"))
 ;;(with-eval-after-load 'magit
 ;;  (require 'forge))
+
+
+;;
+;; Latex support
+;; 
+
+;; Reftex (toc and references)
+(setq reftex-toc-split-windows-horizontally t)
+(setq reftex-toc-max-level 3)
+(setq reftex-toc-split-windows-fraction 0.2)
+
+;; Default pdf viewer
+(setq TeX-view-program-selection '((output-pdf "PDF Tools")))
+
+;; Wrap text 
+(add-hook 'LaTeX-mode-hook #'visual-line-mode)
+
+
+;; OLD ;;
+;;(use-package reftex
+;;  :hook
+;;  (reftex-mode . visual-line-mode)
+;;  :config
+;;  (setq reftex-toc-split-windows-horizontally t)
+;;  (setq reftex-toc-max-level 3)
+;;  (setq reftex-toc-split-windows-fraction 0.2)
+;;  )
+;;
+;;
+;;(use-package auctex
+;;  :init
+;;  (setq TeX-parse-self t ; parse on load
+;;	TeX-auto-save t  ; parse on save
+;;	;; Use hidden directories for AUCTeX files.
+;;	TeX-auto-local ".auctex-auto"
+;;	TeX-style-local ".auctex-style"
+;;	TeX-source-correlate-mode t
+;;	TeX-source-correlate-method 'synctex
+;;	;; Don't start the Emacs server when correlating sources.
+;;	TeX-source-correlate-start-server nil
+;;	;; Automatically insert braces after sub/superscript in `LaTeX-math-mode'.
+;;	TeX-electric-sub-and-superscript t
+;;	;; Just save, don't ask before each compilation.
+;;	TeX-save-query nil
+;;
+;;	TeX-source-correlate-start-server t
+;;	TeX-PDF-mode t
+;;	;; Set pdf viewer
+;;	TeX-view-program-selection '((output-pdf "PDF Tools"))
+;;	reftex-plug-into-AUCTeX t)
+;;  (add-hook 'TeX-after-compilation-finished-functions
+;;	    #'TeX-revert-document-buffer)
+;;  (custom-set-faces
+;;   `(font-latex-slide-title-face ((t (:inherit (variable-pitch font-lock-type-face) :weight bold :height 0.8))))
+;;   )
+;;  :hook
+;;  ((LaTeX-mode . eglot-ensure)
+;;   (LaTeX-mode . LaTeX-math-mode)
+;;   (LaTeX-mode . turn-on-reftex))
+;;  ;;(add-hook 'LaTeX-mode-hook 'eglot-ensure)
+;;  ;;(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+;;  ;;(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+;;  )
+;;
+;;(use-package auctex-latexmk
+;;  :defer t
+;;  :after auctex
+;;  :init
+;;  ;; Pass the -pdf flag when TeX-PDF-mode is active.
+;;  (setq auctex-latexmk-inherit-TeX-PDF-mode t)
+;;  ;; Set LatexMk as the default.
+;;  :config
+;;  (setq TeX-command-default "LatexMk")
+;;  ;; Add LatexMk as a TeX target.
+;;  (auctex-latexmk-setup))
+
+
+
+;;
+;; Zotero support
+;; 
+(setq citar-bibliography org-cite-global-bibliography)
+(setq citar-notes-paths '("~/Zotero/notes"))
+
+;; Org interaction 
+ (with-eval-after-load 'org
+   (setq org-cite-insert-processor 'citar)
+   (setq org-cite-follow-processor 'citar)
+   (setq org-cite-activate-processor 'citar)
+   )
+
+
+;; OLD
+;;(use-package citar
+;;  :bind (("C-c b" . citar-insert-citation)
+;;	 (:map minibuffer-local-map
+;;	 ("M-b" . citar-insert-preset))
+;;	 (:map org-mode-map
+;;	 ("C-c b" . org-cite-insert))
+;;	 )
+;;  :config
+;;  ;; Org cite configuration to use citar
+;;  (setq org-cite-insert-processor 'citar)
+;;  (setq org-cite-follow-processor 'citar)
+;;  (setq org-cite-activate-processor 'citar)
+;;  (setq citar-bibliography org-cite-global-bibliography)
+;;  (setq citar-at-point-function 'embark-act)
+;;  ;; Icons support
+;;  (setq citar-symbols
+;; 	`((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+;; 	  (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+;; 	  (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+;;  (setq citar-symbol-separator "  ")
+;;  ;; Notes
+;;  (setq citar-notes-paths '("~/Zotero/notes"))
+;;  )
