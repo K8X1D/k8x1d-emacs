c028a12e59a4faab102b471144476409e8c3d26e
Kevin Kaiser <k8x1d@proton.me>
2023-04-20 10:45:06 -0400

    test ts support

--- lisp/k8x1d-R.el
+++ lisp/k8x1d-R.el
@@ -1,6 +1,18 @@
 
 
 
+
+;;;; Treesitter support
+;;(use-package r-ts-mode
+;;  :init
+;;  ;; tmp
+;;  (unless (package-installed-p 'r-ts-mode)
+;;    (package-vc-install "https://github.com/sje30/r-ts-mode")
+;;    )
+;;  :mode "\\.R$")
+
+
+
 (use-package ess
   :hook (ess-r-mode . eglot-ensure)
   )
