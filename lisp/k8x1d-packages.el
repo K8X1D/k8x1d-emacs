58cbe217a9b410cc29b57656c997c68ac082c5e7
Kevin Kaiser <k8x1d@proton.me>
2023-04-20 10:46:36 -0400

    add straight support

---
+++ lisp/k8x1d-packages.el
@@ -0,0 +1,20 @@
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Configure use-package to use straight.el by default
+(use-package straight)
+
+(provide 'k8x1d-packages)
+
+
