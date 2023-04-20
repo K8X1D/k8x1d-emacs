52222d78cdcecb1caf23b03600984fe57dd38aea
Kevin Kaiser <k8x1d@proton.me>
2023-04-20 10:44:53 -0400

    Add ts support

--- lisp/k8x1d-julia.el
+++ lisp/k8x1d-julia.el
@@ -5,16 +5,36 @@
 ;; REPL
 (use-package julia-vterm
   :hook (julia-mode . julia-vterm-mode)
-  :bind ("C-c i" . julia-vterm-send-include-buffer-file)
+  :bind 
+  (("C-c o r j" . julia-vterm-repl)
+   :map julia-vterm-mode-map
+   ("C-c i" . julia-vterm-send-include-buffer-file))
   :config
   (setq vterm-kill-buffer-on-exit nil))
 
 ;; LSP
 (use-package eglot-jl
-  :hook (julia-mode . eglot-ensure)
-  :config
+  :straight t
+  :ensure t
+  :hook ((julia-mode . eglot-ensure)
+	 (julia-ts-mode . eglot-ensure))
+  :init
   (setq eglot-connect-timeout 60) ;; prevent eglot timeout
   (eglot-jl-init)
   )
+
+;; Treesitter support
+(use-package julia-ts-mode
+  :straight t
+  :ensure t
+ ;; ;; tmp
+ ;; (unless (package-installed-p 'julia-ts-mode)
+ ;;   (package-vc-install "https://github.com/ronisbr/julia-ts-mode")
+ ;;   )
+  :mode "\\.jl$")
+
+
+
+
 
 (provide 'k8x1d-julia)
