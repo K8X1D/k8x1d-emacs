71e09177aa618dd510772470fe9622d46b055439
Kevin Kaiser <k8x1d@proton.me>
2023-04-27 21:51:41 -0400

    clean

--- lisp/k8x1d-modeline.el
+++ lisp/k8x1d-modeline.el
@@ -12,18 +12,34 @@
   (setq doom-modeline-buffer-encoding nil)
   ;; Custom modal indicator for evil, Under test
   (setq doom-modeline-modal-icon nil)
-  (setq evil-normal-state-tag   (propertize "[Normal]" 'face '((:background ,(doom-color 'green) :foreground ,(doom-color 'bg))))
-	evil-emacs-state-tag    (propertize "[Emacs]" 'face '((:background ,(doom-color 'orange) :foreground ,(doom-color 'bg))))
-	evil-insert-state-tag   (propertize "[Insert]" 'face '((:background ,(doom-color 'red)) :foreground ,(doom-color 'fg)))
-	evil-motion-state-tag   (propertize "[Motion]" 'face '((:background ,(doom-color 'blue)) :foreground ,(doom-color 'fg)))
-	evil-visual-state-tag   (propertize "[Visual]" 'face '((:background ,(doom-color 'grey) :foreground ,(doom-color 'bg))))
-	evil-operator-state-tag (propertize "[Operator]" 'face '((:background ,(doom-color 'violet)))))
+  (setq evil-normal-state-tag   (propertize "(N)" 'face '((:background ,(doom-color 'green) :foreground ,(doom-color 'bg))))
+	evil-emacs-state-tag    (propertize "(E)" 'face '((:background ,(doom-color 'orange) :foreground ,(doom-color 'bg))))
+	evil-insert-state-tag   (propertize "(I)" 'face '((:inverse-video t :background ,(doom-color 'red)) :foreground ,(doom-color 'fg)))
+	evil-motion-state-tag   (propertize "(M)" 'face '((:background ,(doom-color 'blue)) :foreground ,(doom-color 'fg)))
+	evil-visual-state-tag   (propertize "(V)" 'face '((:background ,(doom-color 'grey) :foreground ,(doom-color 'bg))))
+	evil-operator-state-tag (propertize "(O)" 'face '((:background ,(doom-color 'violet)))))
   )
 
+;;(use-package telephone-line
+;;  :hook (after-init . telephone-line-mode)
+;;  :config
+;;  (setq telephone-line-lhs
+;;	'((evil   . (telephone-line-evil-tag-segment))
+;;	  (accent . (telephone-line-vc-segment
+;;		     telephone-line-erc-modified-channels-segment
+;;		     telephone-line-process-segment))
+;;	  (nil    . (telephone-line-minor-mode-segment
+;;		     telephone-line-buffer-segment))))
+;;  (setq telephone-line-rhs
+;;	'((nil    . (telephone-line-misc-info-segment))
+;;	  (accent . (telephone-line-major-mode-segment))
+;;	  (evil   . (telephone-line-airline-position-segment))))
+;;  )
 
 (use-package minions-mode
   :hook (doom-modeline-mode . minions-mode)
-  :config
+  ;; :hook (telephone-line-mode . minions-mode)
+  :init
   (setq minions-mode-line-lighter "...")
   )
 
