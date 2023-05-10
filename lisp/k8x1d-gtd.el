20823fc1bcd59cc1d9c4adceabfba031f69456b0
Kevin Kaiser <k8x1d@proton.me>
2023-05-10 10:12:23 -0400

    upd gtd refile

--- lisp/k8x1d-gtd.el
+++ lisp/k8x1d-gtd.el
@@ -3,23 +3,34 @@
 ;; From https://members.optusnet.com.au/~charles57/GTD/orgmode.html
 ;; https://hamberg.no/gtd
 (use-package org
+  :defer t
   :bind
   (("C-c c" . 'org-capture)
    ("C-c a" . 'org-agenda))
   :config
+  ;; Org agenda configuration
   (setq
    org-agenda-skip-scheduled-if-done t
    org-agenda-skip-deadline-if-done t
-   org-agenda-skip-timestamp-if-done t
+   org-agenda-skip-timestamp-if-done t)
+
+  ;; Org directory
+  (setq 
    org-directory "~/Dropbox/org"
    org-default-notes-file (concat org-directory "/gtd/inbox.org")
    org-todo-keywords
    '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "KILL(k)"))
-   org-agenda-files (file-expand-wildcards (concat org-directory "/gtd/*.org"))
-   ;;org-refile-targets '((org-agenda-files :maxlevel . 3)))
+   org-agenda-files (file-expand-wildcards (concat org-directory "/gtd/*.org")))
+
+  ;; Refile configuration
+  (setq 
    org-refile-targets
    '((nil :maxlevel . 3)
-     (org-agenda-files :maxlevel . 3)))
+     (org-agenda-files :maxlevel . 3))
+   org-refile-use-outline-path t
+   org-refile-active-region-within-subtree 'file
+   org-outline-path-complete-in-steps nil
+   org-refile-allow-creating-parent-nodes t)
   )
 
 
