b07f8eb38f83a5ccfc826ebd273adc3dae6c9722
Kevin Kaiser <k8x1d@proton.me>
2023-05-15 21:05:14 -0400

    upd packages

--- manifests/channels.scm
+++ manifests/channels.scm
@@ -1,19 +1,23 @@
 (list
+ ;; Access to nongnu packages ;; necessery for laptop wifi and cpu updates
  (channel
   (name 'nonguix)
   (url "https://gitlab.com/nonguix/nonguix")
-  ;; Enable signature verification:
+  (branch "master")
+  ;;(commit "9d58bb6e3e93a444d35bf62d4ca98a126ae4f295")
+  (commit "51b5ce6902b6c7bacfc2ed43e8810ac0bca7f8e7")
   (introduction
    (make-channel-introduction
-    "40c48d08cc8c7c82f3212f6695c641cb17ac5d60"
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
     (openpgp-fingerprint
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+ ;; Defaults guix channel
  (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (branch "master")
-  (commit
-   "b08cdfc6d363e9ca63118303b4628542c54a612d")
+  ;;(commit "8b855dc1f4a3e31c002c229ad4d91525f4ce97d3")
+  (commit "b3adba7b5ef3bb56509710b1596225017f1342cc")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
