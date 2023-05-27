40e76749e76ce94c0a147ff928742286839a9c0b
Kevin Kaiser <k8x1d@proton.me>
2023-05-27 15:19:19 -0400

    spell checking

--- lisp/k8x1d-corrector.el
+++ lisp/k8x1d-corrector.el
@@ -12,7 +12,9 @@
   :config
   (setq langtool-http-server-host "localhost"
 	langtool-http-server-port 8081)
-  )
+  (custom-set-faces
+   `(langtool-errline ((nil (:foreground "#ff5f59"))))
+  ))
 
 ;;(use-package eglot-ltex
 ;;  :straight (:type git :host github :repo "emacs-languagetool/eglot-ltex") 
@@ -23,7 +25,41 @@
 ;;  :init
 ;;  (setq eglot-languagetool-server-path (concat user-emacs-directory "/ltex-ls-16.0.0/")))
 
+;;
+;; Spell checking
+;;
+(use-package flyspell
+  :hook (text-mode . flyspell-mode)
+  :config
+  ;; Aspell compatibility
+  ;; see https://www.emacswiki.org/emacs/FlySpell#h5o-8
+  (setq ispell-program-name "aspell"
+	ispell-extra-args '("--sug-mode=ultra"
+			    "--run-together"))
+  )
 
+(use-package flyspell-correct
+  :after flyspell
+  :bind (:map flyspell-mode-map
+	      ("C-c $" . flyspell-correct-at-point))
+  :config
+  ;; Speedup flyspell
+  (setq flyspell-issue-welcome-flag nil
+	flyspell-issue-message-flag nil)
+  ;; leave duplication evaluation to langtool
+  (setq flyspell-mark-duplications-flag nil))
 
+(use-package auto-dictionary-mode
+  :hook (flyspell-mode . auto-dictionary-mode))
+
+;;
+;; Good writing
+;;
+
+(use-package writegood-mode
+  :bind
+  (("C-c e g" . writegood-grade-level)
+   ("C-c e r" . writegood-reading-ease))
+  :hook (text-mode . writegood-mode))
 
 (provide 'k8x1d-corrector)
