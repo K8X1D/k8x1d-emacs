9709172ac8538ba1f0e07f6564841c232e25454b
Kevin Kaiser <k8x1d@proton.me>
2023-06-12 09:40:00 -0400

    add switch theme function

--- lisp/k8x1d-theme.el
+++ lisp/k8x1d-theme.el
@@ -97,12 +97,32 @@
 ;;  (add-to-list 'after-make-frame-functions #'(lambda (x) (load-theme 'modus-vivendi))) 
 ;;  )
 
+
+
+
+
+
 (use-package doom-themes
+  :init
+  (defvar k8x1d/doom-dark-theme 'doom-gruvbox)
+  (defvar k8x1d/doom-light-theme 'doom-gruvbox-light)
+  (defvar k8x1d/actual-theme k8x1d/doom-dark-theme)
+
+  (defun k8x1d/switch-theme (old-theme new-theme)
+    (disable-theme old-theme)
+    (load-theme new-theme t)
+    (setq k8x1d/actual-theme new-theme))
+
+  (defun k8x1d/dark-to-light-to-dark ()
+    (interactive)
+    (if (eq k8x1d/actual-theme k8x1d/doom-dark-theme)
+	(k8x1d/switch-theme k8x1d/doom-dark-theme k8x1d/doom-light-theme)
+      (k8x1d/switch-theme k8x1d/doom-light-theme k8x1d/doom-dark-theme)))
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
 	doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme k8x1d/actual-theme t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
