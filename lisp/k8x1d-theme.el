a1d514cedb0536fea5dcab8e9042709036012d45
Kevin Kaiser <k8x1d@proton.me>
2023-06-12 13:41:33 -0400

    refine switch theme

--- lisp/k8x1d-theme.el
+++ lisp/k8x1d-theme.el
@@ -118,6 +118,8 @@
     (if (eq k8x1d/actual-theme k8x1d/doom-dark-theme)
 	(k8x1d/switch-theme k8x1d/doom-dark-theme k8x1d/doom-light-theme)
       (k8x1d/switch-theme k8x1d/doom-light-theme k8x1d/doom-dark-theme)))
+  :bind
+  ("<f5>" . 'k8x1d/dark-to-light-to-dark)
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
@@ -128,7 +130,7 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
   ;; Brighter comments
-  (setq doom-gruvbox-brighter-comments t)
+  (setq doom-gruvbox-brighter-comments nil)
   )
 
 
