fe609cec35cf23e4c3378316459e858c09fcaa34
Kevin Kaiser <k8x1d@proton.me>
2023-06-18 18:18:26 -0400

    clean

--- lisp/k8x1d-icons.el
+++ lisp/k8x1d-icons.el
@@ -1,19 +1,8 @@
 
 
-
-
-
+;; Icons
 (use-package all-the-icons
-  :if (display-graphic-p)
-      :config
-      ;; Make sure the icon fonts are good to go
-      (set-fontset-font t 'unicode (font-spec :family "all-the-icons") nil 'append)
-      (set-fontset-font t 'unicode (font-spec :family "file-icons") nil 'append)
-      (set-fontset-font t 'unicode (font-spec :family "Material Icons") nil 'append)
-      (set-fontset-font t 'unicode (font-spec :family "github-octicons") nil 'append)
-      (set-fontset-font t 'unicode (font-spec :family "FontAwesome") nil 'append)
-      (set-fontset-font t 'unicode (font-spec :family "Weather Icons") nil 'append))
-
+  :if (display-graphic-p))
 
 
 ;; Add icons to ibuffer
@@ -22,13 +11,13 @@
   (ibuffer-mode . all-the-icons-ibuffer-mode))
 
 
-;; add icons to completion
+;; Add icons to completion
 (use-package all-the-icons-completion
   :hook
   (vertico-mode . all-the-icons-completion-mode))
 
 
-;; Icons support for corfu
+;; Add icons to corfu
 (use-package kind-icon
   :after corfu
   :custom
@@ -37,7 +26,7 @@
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 
-;; add icon to dired
+;; Add icons to dired
 (use-package all-the-icons-dired
   :hook
   (dired-mode . all-the-icons-dired-mode))
