e2f9daffb462a287cc3b2ffada9929260aa4fe9d
Kevin Kaiser <k8x1d@proton.me>
2023-06-18 18:13:45 -0400

    sep buffer per vc

--- lisp/k8x1d-buffers.el
+++ lisp/k8x1d-buffers.el
@@ -1,14 +1,27 @@
-
-
-
-
-
-
-
 ;; Better buffer manager
 (use-package emacs
   :bind
   ("C-x C-b" . ibuffer))
+
+
+;; Group buffers by vc dir in ibuffer
+(use-package ibuffer-vc
+  :init
+  (setq ibuffer-formats
+      '((mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              (vc-status 16 16 :left)
+              " "
+              vc-relative-file)))
+  :hook (ibuffer . (lambda ()
+      (ibuffer-vc-set-filter-groups-by-vc-root)
+      (unless (eq ibuffer-sorting-mode 'alphabetic)
+        (ibuffer-do-sort-by-alphabetic)))))
 
 
 
@@ -19,9 +32,9 @@
 ;;	       (setq ibuffer-filter-groups (ibuffer-project-generate-filter-groups))
 ;;	       (unless (eq ibuffer-sorting-mode 'project-file-relative)
 ;;		 (ibuffer-do-sort-by-project-file-relative)))))
-;;
-;;
-;;
+
+
+
 
 
 
