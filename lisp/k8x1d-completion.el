f1445f434560d9f0c8e36f9410cc0b70f794aa30
Kevin Kaiser <k8x1d@proton.me>
2023-07-22 23:15:24 -0400

    implement lsp-bridge

--- lisp/k8x1d-completion.el
+++ lisp/k8x1d-completion.el
@@ -75,47 +75,51 @@
 
 
 
-;;
-;; Completion interface (pop-up)
-;;
-(use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+;; ;; TODO: evaluate relevance in light of lsp-bridge test
+;; ;;
+;; ;; Completion interface (pop-up)
+;; ;;
+;; (use-package corfu
+;;   ;; Optional customizations
+;;   ;; :custom
+;;   ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+;;   ;; (corfu-auto t)                 ;; Enable auto completion
+;;   ;; (corfu-separator ?\s)          ;; Orderless field separator
+;;   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+;;   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+;;   ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+;;   ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+;;   ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+;;   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
 
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
+;;   ;; Enable Corfu only for certain modes.
+;;   ;; :hook ((prog-mode . corfu-mode)
+;;   ;;        (shell-mode . corfu-mode)
+;;   ;;        (eshell-mode . corfu-mode))
 
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `corfu-exclude-modes'.
-  :init
-  (global-corfu-mode))
+;;   ;; Recommended: Enable Corfu globally.
+;;   ;; This is recommended since Dabbrev can be used globally (M-/).
+;;   ;; See also `corfu-exclude-modes'.
+;;   :config
+;;   (setq corfu-auto t
+;; 	corfu-auto-delay 0.25)
+;;   :hook (after-init . global-corfu-mode)
+;;   )
 
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; TAB cycle if there are only few candidates
-  (setq completion-cycle-threshold 3)
+;; ;; A few more useful configurations...
+;; (use-package emacs
+;;   :init
+;;   ;; TAB cycle if there are only few candidates
+;;   (setq completion-cycle-threshold 3)
 
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
+;;   ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+;;   ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+;;   ;; (setq read-extended-command-predicate
+;;   ;;       #'command-completion-default-include-p)
 
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete))
+;;   ;; Enable indentation+completion using the TAB key.
+;;   ;; `completion-at-point' is often bound to M-TAB.
+;;   (setq tab-always-indent 'complete))
 
 
 
--- lisp/k8x1d-lsp.el
+++ lisp/k8x1d-lsp.el
@@ -1,9 +1,35 @@
-;; Lsp
-(use-package eglot
-  :defer t
-  :init
-  (setq eglot-connect-timeout 60) ;; prevent eglot timeout
-  )
+;;;; Lsp
+;;(use-package eglot
+;;  :defer t
+;;  :init
+;;  (setq eglot-connect-timeout 180) ;; prevent eglot timeout
+;;  )
+
+
+;; FIXME: remplace by guix package
+(add-to-list 'load-path (concat user-emacs-directory "/lsp-bridge"))
+
+
+(use-package yasnippet
+  :hook (after-init . yas-global-mode)) 
+
+(use-package lsp-bridge
+  :hook (after-init . global-lsp-bridge-mode)
+  ;;:config (setq acm-enable-icon t)
+  ;; Don't work, stick with original
+  ;; :config
+  ;;:bind
+  ;;(:map lsp-bridge-mode-map
+  ;;	(("TAB" . acm-select-next)
+  ;;	 ("<backtab>" . acm-select-prev)))
+  ;; :bind (("TAB" . acm-select-next)
+  ;; 	 ("<backtab>" . acm-select-prev)
+  ;; 	 ("RET" . acm-complete))
+  ;;:init
+  ;;(require 'bind-key)
+  ;;(unbind-key "TAB" lsp-bridge-mode-map)
+)
+ 
 
 ;; Diagnostics
 (use-package flymake
