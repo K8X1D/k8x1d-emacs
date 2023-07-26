a161ea8e9a0f324c8035394145335751eab084cd
Kevin Kaiser <k8x1d@proton.me>
2023-07-26 22:21:33 +0200

    adjust keybingings

--- lisp/k8x1d-R.el
+++ lisp/k8x1d-R.el
@@ -23,10 +23,28 @@
 (use-package R-vterm
   :defer t
   ;;:straight (:type git :host gitlab :repo "K8X1D/r-vterm") ;; integrated trought guix
+  :init
+  (defun k8x1d/open-R-repl-at-bottom ()
+     (interactive)
+    (evil-window-split)
+    (evil-window-down 1)
+    (evil-window-set-height 17)
+    (R-vterm-repl)
+    (evil-insert-state)
+    )
   :hook (ess-r-mode . R-vterm-mode)
   :general
   (k8x1d/leader-keys
     "orr" '(R-vterm-repl :which-key "R")
+    )
+  (k8x1d/local-leader-keys
+    :keymaps 'ess-r-mode-map
+    ;;"'" '(julia-vterm-switch-to-repl-buffer :which-key "Open REPL")
+    "'" '(k8x1d/open-R-repl-at-bottom :which-key "Open REPL")
+    "l" '(R-vterm-send-region-or-current-line :which-key "Send line")
+    "b" '(R-vterm-send-buffer :which-key "Send buffer")
+    "f" '(R-vterm-send-include-buffer-file :which-key "Send file")
+    "r" '(R-vterm-send-region-or-current-line :which-key "Send region")
     )
   :bind 
   (("C-c o r r" . R-vterm-repl)
--- lisp/k8x1d-julia.el
+++ lisp/k8x1d-julia.el
@@ -13,10 +13,20 @@
 ;; REPL
 (use-package julia-vterm
   :defer t
+  :init
+  (defun k8x1d/open-julia-repl-at-bottom ()
+     (interactive)
+    (evil-window-split)
+    (evil-window-down 1)
+    (evil-window-set-height 17)
+    (julia-vterm-repl)
+    (evil-insert-state)
+    )
   :general
   (k8x1d/local-leader-keys
     :keymaps 'julia-ts-mode-map
-    "'" '(julia-vterm-switch-to-repl-buffer :which-key "Open REPL")
+    ;;"'" '(julia-vterm-switch-to-repl-buffer :which-key "Open REPL")
+    "'" '(k8x1d/open-julia-repl-at-bottom :which-key "Open REPL")
     "l" '(julia-vterm-send-region-or-current-line :which-key "Send line")
     "b" '(julia-vterm-send-buffer :which-key "Send buffer")
     "f" '(julia-vterm-send-include-buffer-file :which-key "Send file")
