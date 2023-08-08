956f31c3e688d82d790dd02835a45b53306fcbab
Kevin Kaiser <k8x1d@proton.me>
2023-08-08 19:16:09 +0200

    clean

--- lisp/k8x1d-ssh.el
+++ lisp/k8x1d-ssh.el
@@ -1,24 +1,24 @@
 ;; TODO revise names
 
 (setq k8x1d-ssh-addresses
-      '(("Mariadb"  . "/ssh:kkaiser@206.12.89.239:")
-	("Serenity"  . "/ssh:kkaiser@206.12.92.226:")))
+      '(("Mariadb"  . "kkaiser@206.12.89.239")
+	("Serenity"  . "kkaiser@206.12.92.226")))
 
 (defun k8x1d/select-ssh-address ()
-  (list
   (alist-get
    (completing-read "Choose: " k8x1d-ssh-addresses)
-   vms-adresses nil nil 'equal)))
+   k8x1d-ssh-addresses nil nil 'equal))
 
 (defun k8x1d/dired-ssh (vm)
-  (interactive (k8x1d/select-ssh-address))
+  (interactive (list (concat "/ssh:" (k8x1d/select-ssh-address) ":")))
   (find-file vm)
   )
 
 (defun k8x1d/vterm-ssh (vm)
-  (interactive (k8x1d/select-ssh-address))
-  (find-file vm)
+  (interactive (list (concat "ssh" " " (k8x1d/select-ssh-address))))
   (multi-vterm)
+  (vterm-send-string vm)
+  (vterm-send-return)
   )
 
 (provide 'k8x1d-ssh)
