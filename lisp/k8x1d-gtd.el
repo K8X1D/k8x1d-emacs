090cc830fd910c7769b29ec09dfa002fe02990c2
Kevin Kaiser <k8x1d@proton.me>
2023-08-21 14:42:26 +0200

    adapt area of focus

--- lisp/k8x1d-gtd.el
+++ lisp/k8x1d-gtd.el
@@ -129,7 +129,7 @@
   :config
   (setq org-edna-use-inheritance t)
   (setq org-gtd-organize-hooks '(org-gtd-set-area-of-focus org-set-tags-command))
-  ;;(setq org-gtd-areas-of-focus ("Home" "Health" "Family" "Career"))
+  (setq org-gtd-areas-of-focus '("Personnel" "Santé" "Famille" "Emploi" "Recherche" "Développement"))
   :general
   (k8x1d/leader-keys
     "d" '(:ignore t :which-key "GTD")
