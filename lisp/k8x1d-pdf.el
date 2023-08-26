35582044b548209ae2d7fc823dc5b6bcbc5b2633
Kevin Kaiser <k8x1d@proton.me>
2023-08-26 12:05:52 +0200

    clean

--- lisp/k8x1d-pdf.el
+++ lisp/k8x1d-pdf.el
@@ -1,5 +1,10 @@
-;; -*- lexical-binding: t; -*-
+;;; package --- Summary
 
+
+
+;;; Commentary:
+
+;;; Code:
 
 (use-package pdf-tools
   :init
@@ -8,6 +13,4 @@
   )
 
 (provide 'k8x1d-pdf)
-
-
-
+;;; k8x1d-pdf.el ends here
