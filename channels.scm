cd7d5210787d97793b3dcc056e78f3bc245abbab
Kevin Kaiser <k8x1d@proton.me>
2023-08-31 13:04:37 +0200

    reduce

--- channels.scm
+++ channels.scm
@@ -1,13 +1,5 @@
 (list
  (channel
-  (name 'emacs)
-  (url "https://github.com/babariviere/guix-emacs")
-  (introduction
-   (make-channel-introduction
-    "72ca4ef5b572fea10a4589c37264fa35d4564783"
-    (openpgp-fingerprint
-     "261C A284 3452 FB01 F6DF  6CF4 F9B7 864F 2AB4 6F18"))))
- (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (branch "master")
