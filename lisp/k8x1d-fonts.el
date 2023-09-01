3f949c63e67cee1a82e78483caa382bc6f5e4abb
Kevin Kaiser <k8x1d@proton.me>
2023-09-01 13:48:51 +0200

    clean

--- lisp/k8x1d-fonts.el
+++ lisp/k8x1d-fonts.el
@@ -1,25 +1,50 @@
+;;; k8x1d-fonts.el --- Fonts configuration for k8x1d-emacs  -*- lexical-binding: t; -*-
 
-;; Main typeface
+;; Copyright (C) 2023  Kevin Kaiser
 
-;; Fonts
-;;(set-face-attribute 'default nil :font "Fira Code" :height 150)
-(set-face-attribute 'default nil :font "Iosevka Term" :height 120)
+;; Author: Kevin Kaiser <k8x1d@proton.me>
+;; Keywords: extensions
 
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil
-		    :font "Iosevka Term"
-		    :weight 'light
-		    :height 1.0)
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil
-		    :font "Iosevka Aile"
-		    :height 1.0
-		    :weight 'light)
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package emacs
+  :config
+  ;; Main typeface
+  (set-face-attribute 'default nil
+		      :font "Iosevka Term"
+		      :height 120)
+  ;; Set the fixed pitch face
+  (set-face-attribute 'fixed-pitch nil
+		      :font "Iosevka Term"
+		      :weight 'light
+		      :height 1.0)
+  ;; Set the variable pitch face
+  (set-face-attribute 'variable-pitch nil
+		      :font "Iosevka Aile"
+		      :height 1.0
+		      :weight 'light)
+  )
 
 (use-package mixed-pitch
   :hook
   (text-mode . mixed-pitch-mode))
 
 (provide 'k8x1d-fonts)
+;;; k8x1d-fonts.el ends here
