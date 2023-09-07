c2c8a9da0adb33d11ce06b15683c719a935a1c97
Kevin Kaiser <k8x1d@proton.me>
2023-09-07 08:28:37 -0400

    add lambda theme

--- lisp/k8x1d-modeline.el
+++ lisp/k8x1d-modeline.el
@@ -78,46 +78,68 @@
   (setq doom-modeline-workspace-name nil) ;; use tab instead
   )
 
-
-
+;; (use-package lambda-line
+;;   :straight (:type git :host github :repo "lambda-emacs/lambda-line")
+;;   :custom
+;;   (lambda-line-icon-time t) ;; requires ClockFace font (see below)
+;;   (lambda-line-clockface-update-fontset "ClockFaceRectSolid") ;; set clock icon
+;;   (lambda-line-position 'bottom) ;; Set position of status-line
+;;   (lambda-line-abbrev t) ;; abbreviate major modes
+;;   (lambda-line-hspace "  ")  ;; add some cushion
+;;   (lambda-line-prefix t) ;; use a prefix symbol
+;;   (lambda-line-prefix-padding nil) ;; no extra space for prefix
+;;   (lambda-line-status-invert nil)  ;; no invert colors
+;;   (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+;;   (lambda-line-gui-mod-symbol " ⬤")
+;;   (lambda-line-gui-rw-symbol  " ◯")
+;;   (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+;;   (lambda-line-space-bottom -.50)
+;;   (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+;;   :hook ((after-init . lambda-line-mode)
+;; 	 (lambda-line-mode . display-time-mode))
+;;   :config
+;;   ;; set divider line in footer
+;;   (when (eq lambda-line-position 'top)
+;;     (setq-default mode-line-format (list "%_"))
+;;     (setq mode-line-format (list "%_"))))
 
 ;; Custom modeline
 ;; Inspired by https://emacs.stackexchange.com/questions/5529/how-to-right-align-some-items-in-the-modeline
-(use-package emacs
-  :init
-  (defun simple-mode-line-render (left right)
-    "Return a string of `window-width' length.
-Containing LEFT, and RIGHT aligned respectively."
-    (let ((available-width
-	   (- (window-total-width)
-	      (+ (length (format-mode-line left))
-		 (length (format-mode-line right))))))
-      (append left
-	      (list (format (format "%%%ds" available-width) ""))
-	      right)))
-  :config
-  (setq-default
-   ;; (setq
-   mode-line-format
-   '((:eval
-      (simple-mode-line-render
-       ;; Left.
-       (quote ("%e "
-	       evil-mode-line-tag
-	       " "
-	       mode-line-buffer-identification
-	       (vc-mode vc-mode)
-	       " [%l:%c]"
-	       ))
-       ;;
-       ;; Right.
-       (quote (
-	       " "
-	       mode-line-frame-identification
-	       mode-line-misc-info
-	       mode-line-modes
-	       ))))))
-  )
+;; (use-package emacs
+;;   :init
+;;   (defun simple-mode-line-render (left right)
+;;     "Return a string of `window-width' length.
+;; Containing LEFT, and RIGHT aligned respectively."
+;;     (let ((available-width
+;; 	   (- (window-total-width)
+;; 	      (+ (length (format-mode-line left))
+;; 		 (length (format-mode-line right))))))
+;;       (append left
+;; 	      (list (format (format "%%%ds" available-width) ""))
+;; 	      right)))
+;;   :config
+;;   (setq-default
+;;    ;; (setq
+;;    mode-line-format
+;;    '((:eval
+;;       (simple-mode-line-render
+;;        ;; Left.
+;;        (quote ("%e "
+;; 	       evil-mode-line-tag
+;; 	       " "
+;; 	       mode-line-buffer-identification
+;; 	       (vc-mode vc-mode)
+;; 	       " [%l:%c]"
+;; 	       ))
+;;        ;;
+;;        ;; Right.
+;;        (quote (
+;; 	       " "
+;; 	       mode-line-frame-identification
+;; 	       mode-line-misc-info
+;; 	       mode-line-modes
+;; 	       ))))))
+;;   )
 
 (use-package minions-mode
   :straight (:host github :repo "tarsius/minions")
