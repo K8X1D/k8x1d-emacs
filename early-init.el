1b859e4f1268ef8e37873d743ce19e8178c47601
Kevin Kaiser <k8x1d@proton.me>
2023-09-11 08:52:27 -0400

    clean-up

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
 ;;; package --- Summary
 
-;;; Commentary:" (emacs-lisp-checkdoc)
+;;; Commentary:
 
-;;; Code:" (emacs-lisp-checkdoc)
+;;; Code:
 
 
 ;; Inspirations:
@@ -72,16 +72,16 @@
 (set-frame-parameter nil 'alpha-background 80) ; For current frame
 (add-to-list 'default-frame-alist '(alpha-background . 80)) ; For all new frames henceforth
 
-;;;;;; FIXME: daemon mode lacks color, seem to use non emacs 29 config
-;; (if (and (eq window-system 'pgtk) (>= emacs-major-version 29))
-;;     (progn
-;;       (set-frame-parameter nil 'alpha-background 80) ; For current frame
-;;       (add-to-list 'default-frame-alist '(alpha-background . 80)) ; For all new frames henceforth
-;;       )
-;;   (progn
-;;     (set-frame-parameter (selected-frame) 'alpha '(90 . 90)) ; For current frame
-;;     (add-to-list 'default-frame-alist '(alpha . (90 . 90))) ; For all new frames henceforth
-;;     ))
+;;;; FIXME: daemon mode lacks color, seem to use non emacs 29 config
+ (if (and (eq window-system 'pgtk) (>= emacs-major-version 29))
+   (progn
+     (set-frame-parameter nil 'alpha-background 80) ; For current frame
+     (add-to-list 'default-frame-alist '(alpha-background . 80)) ; For all new frames henceforth
+     )
+   (progn
+     (set-frame-parameter (selected-frame) 'alpha '(90 . 90)) ; For current frame
+     (add-to-list 'default-frame-alist '(alpha . (90 . 90))) ; For all new frames henceforth
+     ))
 
 
 ;;; early-init.el ends here
