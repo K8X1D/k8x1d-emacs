bc1867cb30f54db13901b7b35b3da4529c5d1727
Kevin Kaiser <k8x1d@proton.me>
2023-10-02 14:53:12 +0200

    add python-vterm

--- lisp/python-module.el
+++ lisp/python-module.el
@@ -4,13 +4,45 @@
 
 ;;; Code:
 
-;; Python
+;; Syntax highlight
 (use-package python
-  :hook (python-mode . eglot-ensure)
   :config
   (setq python-indent-offset 4)
   (setq python-indent-guess-indent-offset-verbose nil)
-)
+  )
+
+;; REPL
+(use-package python-vterm
+  :vc (:fetcher "gitlab"  :repo "K8X1D/python-vterm")
+  :init
+  (defun k8x1d/open-python-repl-at-bottom ()
+    (interactive)
+    (evil-window-split)
+    (evil-window-down 1)
+    (evil-window-set-height 17)
+    (python-vterm-repl)
+    (evil-insert-state)
+    )
+  :hook ((python-mode . python-vterm-mode))
+  :bind
+  (("C-c o r p" . python-vterm-repl)
+   :map python-vterm-mode-map
+   ("C-c i" . python-vterm-send-include-buffer-file))
+  :config
+  (setq vterm-kill-buffer-on-exit nil)
+  ;; Use radian instead of R
+  (setq python-vterm-repl-program "bpython")
+  (require 'bind-key)
+  (unbind-key "C-c TAB" python-vterm-mode-map)
+  )
+
+
+;; LSP
+(use-package eglot
+  :if (equal lsp-framework "eglot")
+  :hook (python-mode . eglot-ensure)
+  )
+
 
 (provide 'python-module)
 ;;; python-module.el ends here
--- manifest.scm
+++ manifest.scm
@@ -1,24 +1,186 @@
+(use-modules (guix packages)
+	     (guix download)
+	     (guix build-system python)
+	     (guix build-system pyproject)
+	     (gnu packages python)
+	     (gnu packages python-xyz)
+	     (gnu packages libffi)
+	     (gnu packages compression)
+	     (gnu packages check)
+	     ((guix licenses) #:prefix license:)
+	     (gnu packages terminals))
+
+;;
+;; Packages
+;;
+
+(define-public python-backend
+  (package
+   (name "python-backend")
+   (version "0.2.4.1")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "backend" version ".zip"))
+     (sha256
+      (base32 "17p70swiawv0dzmcd04szdaxsfi5dwnfjqjkbcppacf7fkmghqg1"))))
+   (build-system pyproject-build-system)
+    ;; (build-system python-build-system)
+   (arguments
+    `(#:phases
+      (modify-phases %standard-phases
+		     (replace 'build
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     (replace 'install
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     (replace 'check
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     )))
+   (native-inputs (list zip unzip))
+   (home-page "https://github.com/tr4n2uil/backend.django")
+   (synopsis "Backend Utility Tools")
+   (description "Backend Utility Tools")
+   (license license:expat))
+  )
+
+
+(define-public python-rapidfuzz
+  (package
+   (name "python-rapidfuzz")
+   (version "3.3.1")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "rapidfuzz" version))
+     (sha256
+      (base32 "0l5ly2iz2r3cvigbl5d9hf3g7d57ay3kablfd1kpbv8m5y2v70v7"))))
+   (build-system pyproject-build-system)
+   (arguments
+    `(#:phases
+      (modify-phases %standard-phases
+		     (replace 'build
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     (replace 'install
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     (replace 'check
+			      (lambda* (#:key tests? #:allow-other-keys)
+				(setenv "PYTHON" (or (which "python3") (which "python")))
+				))
+		     )))
+   (native-inputs (list python-backend))
+   (home-page "https://github.com/maxbachmann/RapidFuzz")
+   (synopsis "rapid fuzzy string matching")
+   (description "rapid fuzzy string matching")
+   (license license:expat))
+  )
+
+
+(define-public python-rchitect
+(package
+  (name "python-rchitect")
+  (version "0.4.2")
+  (source (origin
+            (method url-fetch)
+            (uri (pypi-uri "rchitect" version))
+            (sha256
+             (base32
+              "1gmab3kxw0zyy2rzc59hjj89zijc27rpzrdfbvh9n5rna5w57564"))))
+  (build-system pyproject-build-system)
+  (propagated-inputs (list python-cffi python-six))
+  (native-inputs (list python-pytest python-pytest-cov python-pytest-mock))
+  (arguments `(#:tests? #f))
+  (home-page "https://github.com/randy3k/rchitect")
+  (synopsis "Mapping R API to Python")
+  (description "Mapping R API to Python")
+  (license #f))
+)
+
+(define-public python-radian
+(package
+  (name "python-radian")
+  (version "0.6.7")
+  (source (origin
+            (method url-fetch)
+            (uri (pypi-uri "radian" version))
+            (sha256
+             (base32
+              "0d2i8dqdjlf3i10q7rhrcflpmrj1xfji8rnh1skm0zhyb33f2zr9"))))
+  (build-system pyproject-build-system)
+  (propagated-inputs (list python-prompt-toolkit python-pygments
+                           python-rchitect))
+  (native-inputs (list python-coverage python-pexpect python-ptyprocess
+                       python-pyte python-pytest))
+  (arguments `(#:tests? #f))
+  (home-page "https://github.com/randy3k/radian")
+  (synopsis "A 21 century R console")
+  (description "This package provides a 21 century R console")
+  (license #f))
+
+)
+
+
+;;
+;; Manifest
 
 
 ;;
 ;; Manifest
 ;;
+(concatenate-manifests
+ (list
   (specifications->manifest
    (list
+    ;; Emacs
+    "emacs-next-pgtk-xwidgets" ;; Emacs text editor with `xwidgets' and `pgtk' support
+
+    ;; Fonts
     "font-iosevka" ;; Iosevka is a slender monospace sans-serif or slab-serif typeface
     "font-iosevka-aile" ;; Iosevka is a slender monospace sans-serif or slab-serif typeface
     "font-iosevka-term" ;; Coders' typeface, built from code
 
+    ;; Vterm dependencies
     "gcc-toolchain" ;; Complete GCC tool chain for C/C++ development
     "cmake" ;; Cross-platform build system
     "libvterm" ;; VT220/xterm/ECMA-48 terminal emulator library
-    "python-lsp-server" ;; Python implementation of the Language Server Protocol
-    "emacs-next-pgtk-xwidgets" ;; Emacs text editor with `xwidgets' and `pgtk' support
 
-    "texlive-bin" ;; TeX Live, a package of the TeX typesetting system
-    "texlive-digestif" ;; Editor plugin for LaTeX, ConTeXt etc.
-    "texlive-dvipng" ;; DVI to PNG/GIF converter
-    "texlive-luatex" ;; Extended version of pdfTeX using Lua
-    "r-languageserver" ;; Language Server for R
+    ;; Utilities
+    "fd" ;; Simple, fast and user-friendly alternative to find
+
+
+    ;; Markdown
+    "markdown" ;; Text-to-HTML conversion tool
+
+    ;; Julia
+    "julia" ;; High-performance dynamic language for technical computing
+
+
+    ;; Python
+    "python-lsp-server" ;; Python implementation of the Language Server Protocol
+    "bpython" ;; Fancy interface to the Python interpreter
+
+
     "qtlanguageserver" ;;  Implementation of the Language Server Protocol for Qt
+
+    ;; LSP Bridge depedencies
+    "python" ;; High-level, dynamically-typed programming language
+    "python-epc" ;; Remote procedure call (RPC) stack for Emacs Lisp and Python
+    "python-orjson" ;; Python JSON library supporting dataclasses, datetimes, and numpy
+    "python-sexpdata" ;; S-expression parser for Python
+    "python-six" ;; Python 2 and 3 compatibility utilities
+    "python-paramiko"  ;; SSHv2 protocol library
     ))
+  (packages->manifest (list
+		       python-radian
+		       python-rapidfuzz
+		       ))
+  ))
