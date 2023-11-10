084cec9085c340eaea0cdacf1231ef5095eab49f
Kevin Kaiser <k8x1d@proton.me>
2023-11-10 11:05:54 -0500

    init nix support

---
+++ lisp/nix-module.el
@@ -0,0 +1,24 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;; Syntax Hightlight
+(use-package nix-mode)
+
+(use-package nix-buffer)
+(use-package nix-env-install)
+(use-package nix-modeline)
+(use-package nix-sandbox)
+(use-package nix-ts-mode)
+
+
+
+
+
+
+
+
+(provide 'nix-module)
+;;; nix-module.el ends here
