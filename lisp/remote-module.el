84b1a6c4498e6a19becdab10e2977cfe45f5b9f5
Kevin Kaiser <k8x1d@proton.me>
2024-03-11 08:44:36 +0100

    add predefined ssh connection infos

--- lisp/remote-module.el
+++ lisp/remote-module.el
@@ -17,6 +17,32 @@
   (setq tramp-use-ssh-controlmaster-options nil)
   )
 
+;; Predefined connections
+(use-package tramp
+  :init
+  (defvar k8x1d-ssh-addresses
+    '(("Mariadb"  . "kkaiser@206.12.89.239")
+      ("Serenity_kkaiser"  . "kkaiser@206.12.92.226")
+      ("Shelly"  . "k8x1d@67.68.116.59")
+      ("Serenity_ubuntu"  . "ubuntu@206.12.92.226")
+      ("Groudon" . "kkaiser@130.104.60.82")))
+  :config
+  (defun k8x1d/select-ssh-address ()
+    (alist-get
+     (completing-read "Choose: " k8x1d-ssh-addresses)
+     k8x1d-ssh-addresses nil nil 'equal))
+
+  (defun k8x1d/dired-ssh (vm)
+    (interactive (list (concat "/ssh:" (k8x1d/select-ssh-address) ":")))
+    (find-file vm)
+    )
+  ;; Sometimes, cannot insert back in evil mode. Test alternative
+  (defun k8x1d/eat-ssh (vm)
+    (interactive (list (concat "ssh" " " (k8x1d/select-ssh-address))))
+    (eat vm)
+    )
+  )
+
 ;; Docker interaction (inspiration: https://www.emacswiki.org/emacs/TrampAndDocker)
 (use-package tramp
   :config
