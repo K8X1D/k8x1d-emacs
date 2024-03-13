cb9f14226fe9211d8ce33759d4235e145c0af16c
Kevin Kaiser <k8x1d@proton.me>
2024-03-13 09:39:51 +0100

    add customization

--- lisp/gui/keybindings-module.el
+++ lisp/gui/keybindings-module.el
@@ -1,9 +1,10 @@
-
-
-
 ;; Show keybindings 
 (use-package which-key
-  :hook (after-init . which-key-mode))
+  :hook (after-init . which-key-mode)
+  :config 
+  (setq which-key-separator ": ")
+  (setq which-key-min-display-lines 20)
+  )
 
 
 (provide 'keybindings-module)
