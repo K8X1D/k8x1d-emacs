9fcd2bc680ca048c3717d69d00b3f8722167834b
Kevin Kaiser <k8x1d@proton.me>
2024-03-28 08:57:14 +0100

    add forge support

--- lisp/prog/version-control-module.el
+++ lisp/prog/version-control-module.el
@@ -1,10 +1,23 @@
 ;; Version control management
 (use-package magit
-  :bind ("C-c g" . magit-status))
+  :bind (("C-c g g" . magit-status)))
 
 ;; Issue creation/interaction
 (use-package forge
-  :after magit)
+  :bind
+  (("C-c g a" . forge-add-repository)
+  ("C-c g p i" . forge-pull)
+  ("C-c g c i" . forge-create-issue)
+  ("C-c g c p" . forge-create-post)
+  ("C-c g l i" . forge-list-issues)
+  ("C-c g l r" . forge-list-repository))
+  :config
+  ;; (add-to-list 'forge-alist '("gitlab.cirst.ca" "gitlab.cirst.ca/api/v4" "gitlab.cirst.ca:8822" forge-gitlab-repository))
+  (add-to-list 'forge-alist '("gitlab.cirst.ca" "gitlab.cirst.ca/api/v4" "gitlab.cirst.ca" forge-gitlab-repository)) 
+  (setq auth-sources '("~/.authinfo.gpg"))
+  )
+;; ssh://git@gitlab.cirst.ca:8822/bin-phun/ofdig.git
+;; ssh://gitlab.cirst.ca:8822/
 
 ;; List todo in magit buffer
 (use-package magit-todos
