9b260155d9c9c2cc19d1c174914c77d9bfcad5a4
Kevin Kaiser <k8x1d@proton.me>
2024-03-28 08:57:51 +0100

    don't ask for evaluation

--- lisp/prog/notebook-module.el
+++ lisp/prog/notebook-module.el
@@ -5,6 +5,7 @@
   ;; ensure precise linkage between notebook and file
   (require 'org-id)
   (setq org-id-link-to-org-use-id t)
+  (setq org-confirm-babel-evaluate nil)
   )
 
 (provide 'notebook-module)
