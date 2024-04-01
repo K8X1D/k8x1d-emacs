0b71920d676d85775884c543f8a8f5b09ba793aa
Kevin Kaiser <k8x1d@proton.me>
2024-04-01 12:19:00 +0200

    stop eldoc from polluting echo-bar

--- lisp/documentation-module.el
+++ lisp/documentation-module.el
@@ -1,16 +1,18 @@
-(use-package eldoc)
-
-;;(use-package eldoc-overlay
-;;  :hook (after-init . eldoc-overlay-mode))
-
-(use-package eldoc-box
-  :if (and (string= k8x1d/lsp-backend "eglot") k8x1d/posframe-support)
-  :after eglot
-  :bind (:map eglot-mode-map
-	      ([remap eldoc-doc-buffer] . eldoc-box-help-at-point))
+(use-package eldoc
   :config
   (setq eldoc-echo-area-prefer-doc-buffer t)
+  (setq eldoc-echo-area-use-multiline-p nil)
+  ;; (add-hook 'eglot-managed-mode-hook (lambda () (eldoc-mode -1)))
+
+  (setq eldoc-display-functions '(eldoc-display-in-buffer))
   )
+
+(use-package eldoc-box
+ :if (and (string= k8x1d/lsp-backend "eglot") k8x1d/posframe-support)
+ :after eglot
+ :bind (:map eglot-mode-map
+	      ([remap eldoc-doc-buffer] . eldoc-box-help-at-point))
+ )
 
 (provide 'documentation-module)
 
