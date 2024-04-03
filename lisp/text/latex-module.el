4b24d6e6cd52e07b7c0563cf40d719577e0d0ad6
Kevin Kaiser <k8x1d@proton.me>
2024-04-03 10:35:53 +0200

    add reftex support

--- lisp/text/latex-module.el
+++ lisp/text/latex-module.el
@@ -13,4 +13,17 @@
   (unbind-key "m-n" evil-tex-mode-map) ;; clean keys for lsp-bridge in latex
   )
 
+;; Navigation
+(use-package reftex
+  :hook
+  ((LaTeX-mode . 'turn-on-reftex)
+   (reftex-mode . visual-line-mode))
+  :config
+  (setq reftex-toc-split-windows-horizontally t)
+  (setq reftex-toc-max-level 3)
+  ;; (setq reftex-plug-into-AUCTeX t)
+  (setq reftex-default-bibliography k8x1d/bibliography)
+  (setq reftex-toc-split-windows-fraction 0.2))
+
+
 (provide 'latex-module)
