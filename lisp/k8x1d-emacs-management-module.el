abcd0bbf407a6ff5e514c8ed16fe787a894d04cf
Kevin Kaiser <k8x1d@proton.me>
2024-04-03 10:37:32 +0200

    add upgrade

--- lisp/k8x1d-emacs-management-module.el
+++ lisp/k8x1d-emacs-management-module.el
@@ -1,18 +1,14 @@
-
-
-
-
-
-
-
-
 (use-package emacs
   :init
   (defun k8x1d/update ()
     (interactive)
     (async-shell-command "k8x1d-emacs -u"))
+  (defun k8x1d/upgrade ()
+    (interactive)
+    (async-shell-command "k8x1d-emacs -U"))
   :bind
-  ("C-c s u" . k8x1d/update)
+  (("C-c s u" . k8x1d/update)
+  ("C-c s U" . k8x1d/upgrade))
 )
 
 (provide 'k8x1d-emacs-management-module)
