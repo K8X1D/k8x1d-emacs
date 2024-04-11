aba112ea2e20021d49991049477c5939f63508c6
Kevin Kaiser <k8x1d@proton.me>
2024-04-11 11:00:47 +0200

    add countdown for time left

--- lisp/gui/modeline-module.el
+++ lisp/gui/modeline-module.el
@@ -1,4 +1,3 @@
-
 ;; Modeline definition
 (use-package emacs
   :init
@@ -58,5 +57,41 @@
   :hook ((eat-mode . hide-mode-line-mode)
 	 (dired-sidebar-mode . hide-mode-line-mode)
 	 (special-mode . hide-mode-line-mode)))
+
+(use-package memento-mori
+  :config
+  (defun memento-mori--assert-death-date ()
+    "Ensure that `memento-mori-death-date' has been set."
+    (when (or (null memento-mori-death-date)
+	      (equal "" memento-mori-death-date))
+      (error "Death date not set.  Try M-x customize-group memento-mori")))
+  (defun memento-mori--death-time ()
+    "Return your death time in `encode-time' format.
+The death time is parsed from `memento-mori-death-date' using
+`parse-time-string'.  An error is signaled if it is not valid."
+    (memento-mori--assert-death-date)
+    (let* ((decoded (parse-time-string
+		     (if (stringp memento-mori-death-date)
+			 memento-mori-death-date
+		       "")))
+	   (day (elt decoded 3))
+	   (month (elt decoded 4))
+	   (year (elt decoded 5)))
+      (unless (and day month year)
+	(error "Cannot parse death date %S" memento-mori-death-date))
+      (encode-time 0 0 0 day month year)))
+  (defun memento-mori--life-expectency ()
+    "Return your age in years.
+This is a floating point number based on `memento-mori-death-date'."
+    (/ (truncate (float-time
+		     (time-subtract (memento-mori--death-time) (current-time))))
+	  (* 60 60 24 365.2425)))
+  (defun memento-mori--update ()
+    "Update `memento-mori-string' based on the current time."
+    (setq memento-mori-string
+	  (format "[Time left: %.2f years]" (memento-mori--life-expectency))))
+  (setq memento-mori-death-date "2067-06-22")
+  :hook (after-init . memento-mori-mode)
+  )
 
 (provide 'modeline-module)
