e29a020cab6bf894d611fa8f3dff39f3e68588cf
Kevin Kaiser <k8x1d@proton.me>
2024-04-11 11:01:04 +0200

    adjust julia lsp suport

--- lisp/prog/julia-module.el
+++ lisp/prog/julia-module.el
@@ -105,7 +105,55 @@
   :hook (julia-ts-mode . eglot-ensure)
   :config
   (add-to-list 'eglot-server-programs
-	       '(julia-ts-mode . ("julia" "--startup-file=no" "-e" "using LanguageServer; runserver()")))
+	       ;; From nvim-lspconfig, https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#julials
+    ;; 	       '(julia-ts-mode . (
+    ;; 				  "julia"
+    ;; 				  "--startup-file=no"
+    ;; 				  "--history-file=no"
+    ;; 				  "-e"
+    ;; 				  "# Load LanguageServer.jl: attempt to load from ~/.julia/environments/nvim-lspconfig
+    ;; # with the regular load path as a fallback
+    ;;   ls_install_path = joinpath(
+    ;; 				 get(DEPOT_PATH, 1, joinpath(homedir(), \".julia\")),
+    ;; 				 \"environments\", \"nvim-lspconfig\"
+    ;; 				 )
+    ;; 				  pushfirst!(LOAD_PATH, ls_install_path)
+    ;; 				  using LanguageServer
+    ;; 				  popfirst!(LOAD_PATH)
+    ;; 				  depot_path = get(ENV, \"JULIA_DEPOT_PATH\", \"\")
+    ;; 				  project_path = let
+    ;; 				  dirname(something(
+    ;; 						    ## 1. Finds an explicitly set project (JULIA_PROJECT)
+    ;; 						    Base.load_path_expand((
+    ;; 									   p = get(ENV, \"JULIA_PROJECT\", nothing);
+    ;; 									   p === nothing ? nothing : isempty(p) ? nothing : p
+    ;;         )),
+    ;;         ## 2. Look for a Project.toml file in the current working directory,
+    ;;         ##    or parent directories, with $HOME as an upper boundary
+    ;;         Base.current_project(),
+    ;;         ## 3. First entry in the load path
+    ;;         get(Base.load_path(), 1, nothing),
+    ;;         ## 4. Fallback to default global environment,
+    ;;         ##    this is more or less unreachable
+    ;;         Base.load_path_expand(\"@v#.#\"),
+    ;;     ))
+    ;; end
+    ;; @info \"Running language server\" VERSION pwd() project_path depot_path
+    ;; server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+    ;; server.runlinter = true
+    ;; run(server)"
+    ;; 				  ))
+
+	       '(julia-ts-mode . (
+				  "julia"
+				  "--startup-file=no"
+				  "--history-file=no"
+				  ;; "--project=/home/k8x1d/.julia/environments/emacs"
+				  "--project=/home/k8x1d/.julia/environments/nvim-lspconfig"
+				  "-e"
+				  "using LanguageServer; runserver()"
+	       ))
+	       )
   )
   
 ;; Documentation
