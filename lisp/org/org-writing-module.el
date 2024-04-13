cb9022fe37c481f340ce22df2d2bac8604b18e93
Kevin Kaiser <k8x1d@proton.me>
2024-04-13 12:52:43 +0200

    add citar support

--- lisp/org/org-writing-module.el
+++ lisp/org/org-writing-module.el
@@ -5,5 +5,13 @@
   :hook (org-mode . toc-org-mode))
 
 
+;; Citar integration
+(use-package citar-org
+  :after org
+  :custom
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar))
+
 
 (provide 'org-writing-module)
