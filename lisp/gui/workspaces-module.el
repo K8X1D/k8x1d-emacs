50a50b9933d7b2b343bb1cc0563245097017f36d
Kevin Kaiser <k8x1d@proton.me>
2024-04-14 11:04:32 +0200

    upd

--- lisp/gui/workspaces-module.el
+++ lisp/gui/workspaces-module.el
@@ -14,8 +14,7 @@
     (custom-set-faces
      `(tab-bar ((t (:underline (:color ,(doom-color 'green)  :style line :position t) :foreground ,(doom-color 'green)))))
      `(tab-bar-tab ((t (:foreground ,(doom-color 'green) :background ,(doom-color 'bg) :weight bold :height 1.3 :box (:line-width (1 . 1) :color ,(doom-color 'green) :style flat-button) :underline (:color ,(doom-color 'bg)  :style line :position t)))))
-
-     `(tab-bar-tab-inactive ((t (:foreground ,(doom-color 'blue) :background ,(doom-color 'bg-alt) :slant italic :height 1.3))))
+     `(tab-bar-tab-inactive ((t (:foreground ,(doom-color 'blue) :background ,(doom-color 'bg-alt) :height 1.3))))
      ))
   :hook (after-init . tab-bar-mode)
   :config
@@ -24,7 +23,7 @@
   (setq tab-bar-close-button-show nil)
   (setq tab-bar-new-tab-choice "*scratch*")
   (setq tab-bar-auto-width-max '(100 10))
-  (setq tab-bar-new-tab-to 'leftmost)
+  (setq tab-bar-new-tab-to 'rightmost)
   (setq tab-bar-select-tab-modifiers '(meta))
   (setq tab-bar-tab-name-format-function #'k8x1d/tab-bar-tab-name-format)
   (setq tab-bar-format '(tab-bar-format-history tab-bar-format-tabs tab-bar-separator tab-bar-format-align-right tab-bar-format-global tab-bar-separator))
