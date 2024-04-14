d4afa31e1803dc680997cb4fce9bce7e8c64101f
Kevin Kaiser <k8x1d@proton.me>
2024-04-14 09:29:09 +0200

    init

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (tab-rename "K8X1D-Emacs"))
+	 (eval . (tab-group "Devel")))))
