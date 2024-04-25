20b5b71dcb74df491c42257819566c2c993026b9
Kevin Kaiser <k8x1d@proton.me>
2024-04-25 09:46:18 +0200

    add ltex support

--- lisp/text/text-module.el
+++ lisp/text/text-module.el
@@ -7,4 +7,16 @@
   :hook ((text-mode . mixed-pitch-mode)
          (markdown-mode . mixed-pitch-mode)))
 
+
+(use-package eglot-ltex
+  :hook (text-mode . (lambda ()
+                       (require 'eglot-ltex)
+                       (eglot-ensure)))
+  :init
+  (setq eglot-ltex-server-path "/home/k8x1d/.config/k8x1d-emacs/.cache/ltex-ls-15.2.0")
+  (setq eglot-connect-timeout 180)
+  (setq eglot-ltex-communication-channel 'stdio))         ; 'stdio or 'tcp
+
+
+
 (provide 'text-module)
