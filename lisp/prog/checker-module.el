0c22147bdba9895c190501407038ed8123af6ea0
Kevin Kaiser <k8x1d@frame13.telenet.be>
2024-04-28 16:37:05 +0200

    use margin + general keybindings

--- lisp/prog/checker-module.el
+++ lisp/prog/checker-module.el
@@ -15,8 +15,39 @@
 (use-package flymake-posframe
   :if k8x1d/posframe-support
   :hook (flymake-mode . flymake-posframe-mode)
-  :config
-
   )
+
+;; Indicator in the margin
+(use-package flymake-margin
+  :hook ((flymake-mode . flymake-margin-mode)
+	 (flymake-margin-mode . (lambda () (progn
+					     ;; (setq left-margin-width 2)
+					     (setq right-margin-width 3)
+					     (set-window-buffer nil (current-buffer)))))
+	 )
+  :config
+  ;; (setq flymake-margin-error-symbol " ")
+  ;; (setq flymake-margin-warning-symbol " ")
+  ;; (setq flymake-margin-note-symbol " ")
+  (setq flymake-margin-error-symbol "⮾")
+  (setq flymake-margin-warning-symbol "△")
+  (setq flymake-margin-note-symbol "?")
+  (setq flymake-margin-side 'right)
+  )
+
+
+;; Keybindings
+(use-package flymake
+  :if k8x1d/use-general-keybindings
+  :general
+  (k8x1d/local-leader-keys
+   :keymaps 'flymake-mode-map
+   "d" '(:ignore t :which-key "Diagnostics")
+   "db" '(flymake-show-buffer-diagnostics :which-key "Buffer")
+   "dp" '(flymake-goto-prev-error :which-key "Previous")
+   "dn" '(flymake-goto-next-error :which-key "Next")
+   )
+  )
+
 
 (provide 'checker-module)
