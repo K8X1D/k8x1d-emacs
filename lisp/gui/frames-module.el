ee50ffc59fa82cf33a71f1b2cd751c15ed580c5b
Kevin Kaiser <k8x1d@frame13.telenet.be>
2024-04-28 16:39:05 +0200

    upd

---
+++ lisp/gui/frames-module.el
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+(modify-all-frames-parameters
+ '(
+   (left-fringe . 15)
+   (right-fringe . 15)
+   ))
+
+(provide 'frames-module)
---
+++ lisp/management/project-module.el
@@ -0,0 +1,19 @@
+;; consult integration
+(use-package consult-jump-project
+  :custom (consult-jump-direct-jump-modes '(dired-mode))
+  :bind ("C-x p j" . consult-jump-project))
+
+;; Keybindings
+(use-package project
+  :if k8x1d/use-general-keybindings
+  :general
+  (k8x1d/leader-keys
+    "p" '(:keymap project-prefix-map
+		  :which-key "Project")
+    )
+  )
+
+
+
+
+(provide 'project-module)
