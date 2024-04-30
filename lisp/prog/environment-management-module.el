a85a41585835ef08d706ff511a0bfbd61fb83ba2
Kevin Kaiser <k8x1d@proton.me>
2024-04-30 21:58:13 +0200

    change to direnv for async evaluation

--- lisp/prog/environment-management-module.el
+++ lisp/prog/environment-management-module.el
@@ -1,5 +1,12 @@
 ;; Support direnv (buffer-locally)
-(use-package envrc
-  :hook (after-init . envrc-global-mode))
+;; (use-package envrc
+;;   :hook (after-init . envrc-global-mode))
+
+
+;; Async evaluation, but global, in test...
+(use-package direnv
+ :config
+ (direnv-mode))
+
 
 (provide 'environment-management-module)
