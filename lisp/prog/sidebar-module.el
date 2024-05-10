64c2a89eb852586cc3c5c2d901f7d134096bacf0
Kevin Kaiser <k8x1d@proton.me>
2024-05-10 13:20:17 +0200

    repair font error

--- lisp/prog/sidebar-module.el
+++ lisp/prog/sidebar-module.el
@@ -13,7 +13,8 @@
     ;; (interactive)
     ;; (dired-sidebar-toggle-sidebar)
     ;; (ibuffer-sidebar-toggle-sidebar))
-  ;; :hook (dired-sidebar-mode . visual-line-mode)
+    ;; :hook (dired-sidebar-mode . visual-line-mode)
+    :custom-face  (dired-sidebar-face ((t (:weight bold))))
     :config
     (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
     (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
@@ -21,7 +22,8 @@
     (setq dired-sidebar-subtree-line-prefix "__")
     ;; (setq dired-sidebar-theme 'vscode)
     (setq dired-sidebar-use-term-integration t)
-    (setq dired-sidebar-use-custom-font t))
+    ;; (setq dired-sidebar-use-custom-font t)
+    )
 
 (use-package minimap
   :bind (:map project-prefix-map
