1b8f63961007e56ca9ab1082c44f0b29ae38d4a7
Kevin Kaiser <k8x1d@proton.me>
2024-05-30 10:21:39 +0200

    corr

--- lisp/prog/tree-sitter-module.el
+++ lisp/prog/tree-sitter-module.el
@@ -4,6 +4,8 @@
   (treesit-auto-install 'prompt)
   (treesit--install-language-grammar-out-dir-history (concat user-emacs-directory "tree-sitter"))
   :config
+  ;; Remove unbuildable languages
+  (delete 'yaml treesit-auto-langs)
 
 ;; Its value is ("~/.config/emacs-test/tree-sitter")
   (treesit-auto-add-to-auto-mode-alist 'all))
