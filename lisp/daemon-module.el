59126be3160a10f48fe9b7ad1c5d19708eedaecf
Kevin Kaiser <k8x1d@proton.me>
2024-05-30 10:22:07 +0200

    corr

--- lisp/daemon-module.el
+++ lisp/daemon-module.el
@@ -3,7 +3,7 @@
 	  (lambda (frame) 
 	    (with-selected-frame frame
 	      (k8x1d/set-org-modern-colors)
-	      (k8x1d/set-tab-bar-colors)
+	      ;; (k8x1d/set-tab-bar-colors)
 	      (k8x1d/set-window-divider-colors)
 	      )))
 
